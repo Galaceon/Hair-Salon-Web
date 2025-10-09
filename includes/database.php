a0515dc40ea46459c748d5e2a0ebbefd5120eea5
Galaceon <antoniogarciatr@gmail.com>
2025-10-09 03:24:52 +0200

    Update .gitignore, composer files, and database connection to use environment variables

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,21 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'root', 'appsalon_mvc');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$db = mysqli_connect(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME']
+);
 
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
