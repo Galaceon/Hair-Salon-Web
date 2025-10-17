e420c0fca7eb7f6b6382542504f9806265734f84
Galaceon <antoniogarcialtr@gmail.com>
2025-10-17 22:08:28 +0200

    feat: mostrarResumen
    -feat: mostrarResumen: shows summary of the service or not according to the data
    
    - fix: Active Record name value query error
    
    -  docs: comments for cita index.php

--- src/js/app.js
+++ src/js/app.js
@@ -28,6 +28,8 @@ function iniciarApp() {
     nombreCliente(); // Añade el nombre del cliente al objeto de cita
     seleccionarFecha() // Añade la fecha de la cita en el objeto
     seleccionarHora() //Añade la hora de la cita en el objeto
+
+    mostrarResumen(); // Muestra el resumen de la cita
 }
 
 // Muestra la sección que corresponde al paso
@@ -80,6 +82,8 @@ function botonesPaginador() {
     } else if(paso === 3) {
         paginaAnterior.classList.remove('ocultar');
         paginaSiguiente.classList.add('ocultar');
+
+        mostrarResumen();
     } else {
         paginaAnterior.classList.remove('ocultar');
         paginaSiguiente.classList.remove('ocultar');
@@ -244,3 +248,12 @@ function mostrarAlerta(mensaje, tipo) {
     }, 3500);
 }
 
+function mostrarResumen() {
+    const resumen = document.querySelector('.contenido-resumen');
+
+    if(Object.values(cita).includes('')) {
+        console.log('Hacen falta datos');
+    } else {
+        console.log('Conseguido')
+    }
+}
--- views/cita/index.php
+++ views/cita/index.php
@@ -2,16 +2,21 @@
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
 <div id="app">
+    <!-- Navegación de los pasos -->
     <nav class="tabs">
         <button type="button" data-paso="1">Servicios</button>
         <button type="button" data-paso="2">Información Cita</button>
         <button type="button" data-paso="3">Resumen</button>
     </nav>
+
+    <!-- Paso 1: Servicios elegidos de la cita -->
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
         <p class="text-center">Elige tus Servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
+
+    <!-- Paso 2: Datos de la cita -->
     <div id="paso-2" class="seccion">
         <h2>Tus Datos y Cita</h2>
         <p class="text-center">Coloca tus datos y fecha de tu cita</p>
@@ -19,7 +24,7 @@
         <form class="formulario">
             <div class="campo">
                 <label for="nombre">Nombre</label>
-                <input type="text" id="nombre" placeholder="Tu nombre" value="<?php echo$nombre; ?>" disabled>
+                <input type="text" id="nombre" placeholder="Tu nombre" value="<?php echo $nombre; ?>" disabled>
             </div>
 
             <div class="campo">
@@ -33,11 +38,14 @@
             </div>
         </form>
     </div>
-    <div id="paso-3" class="seccion">
+
+    <!-- Paso 3: Resumen de la cita -->
+    <div id="paso-3" class="seccion contenido-resumen">
         <h2>Resumen</h2>
         <p class="text-center">Verifica que la información sea correcta</p>
     </div>
 
+    <!-- Paginación -->
     <div class="paginacion">
         <button id="anterior" class="boton">&laquo; Anterior</button>
         <button id="siguiente" class="boton">Siguiente &raquo;</button>
