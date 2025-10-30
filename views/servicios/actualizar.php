163689d59994c858e90ef7c07c9f3c63086c62cd
Galaceon <antoniogarcialtr@gmail.com>
2025-10-30 23:08:03 +0100

    creating views for services

---
+++ views/servicios/actualizar.php
@@ -0,0 +1,6 @@
+<h1 class="nombre-pagina">Actualizar Servicios</h1>
+<p class="descripcion-pagina">Modifica los Servicios en los siguientes campos</p>
+
+<?php
+    include_once __DIR__ . '/../templates/barra.php';
+?>
